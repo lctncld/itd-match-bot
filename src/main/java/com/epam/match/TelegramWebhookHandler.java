93196235914d79e0656b15d95eb355a53434c402
Andrey Volodin <andrey_volodin@epam.com>
2018-02-27 17:59:45 +0300

    Send keyboard action to retrieve location

--- src/main/java/com/epam/match/TelegramWebhookHandler.java
+++ src/main/java/com/epam/match/TelegramWebhookHandler.java
@@ -3,6 +3,8 @@ package com.epam.match;
 import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -26,7 +28,14 @@ public class TelegramWebhookHandler {
         .map(BotUtils::parseUpdate)
         .flatMap(update -> {
           Message message = update.message();
-          SendResponse response = bot.execute(new SendMessage(message.chat().id(), message.text()));
+
+          KeyboardButton shareLocation = new KeyboardButton("Share My Location")
+              .requestLocation(true);
+
+          SendMessage req = new SendMessage(message.chat().id(), "Share your location to proceed")
+              .replyMarkup(new ReplyKeyboardMarkup(new KeyboardButton[] { shareLocation }));
+
+          SendResponse response = bot.execute(req);
           log.info("sendMessage {}", response);
           return Mono.empty();
         });
