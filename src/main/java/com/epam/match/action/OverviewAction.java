35a1ab1ecf63c0b38ba5605b90d05c517a231673
Andrey Volodin <andrey_volodin@epam.com>
2018-02-28 14:56:23 +0300

    Respond to overview command with a callback id

--- src/main/java/com/epam/match/action/OverviewAction.java
+++ src/main/java/com/epam/match/action/OverviewAction.java
@@ -1,20 +1,19 @@
 package com.epam.match.action;
 
-import com.pengrad.telegrambot.model.request.ReplyKeyboardRemove;
+import com.pengrad.telegrambot.request.AnswerCallbackQuery;
 import com.pengrad.telegrambot.request.BaseRequest;
-import com.pengrad.telegrambot.request.SendMessage;
 
 public class OverviewAction implements Action {
 
-  private final Long chatId;
+  private final String callbackQueryId;
 
-  public OverviewAction(Long chatId) {
-    this.chatId = chatId;
+  public OverviewAction(String callbackQueryId) {
+    this.callbackQueryId = callbackQueryId;
   }
 
   @Override
   public BaseRequest toCommand() {
-    return new SendMessage(chatId, "This is just a Demo! Try another button")
-        .replyMarkup(new ReplyKeyboardRemove());
+    return new AnswerCallbackQuery(callbackQueryId)
+        .text("This is just a Demo! Try another button");
   }
 }
